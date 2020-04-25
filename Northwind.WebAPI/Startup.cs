1a7eabc7b7ea81237d5e7b2d161ce323e5872475
NickLiu <stalinmalcontent2009@gmail.com>
2020-04-25 15:22:55 +0800

    Add Northwind Context in StartUp Injection and ConnectionString in appSetting

--- Northwind.WebAPI/Startup.cs
+++ Northwind.WebAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +26,10 @@ namespace Northwind.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DbModels.Model.NorthwindContext>(options =>
+       options.UseSqlServer(
+           Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
